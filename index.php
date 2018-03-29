9c42ac14252bfc9a751ef4a07731aed47e89ddd2
Dario Gallegos <dariogal@ucm.es>
2018-03-29 20:14:17 +0200

    Actualizo index
    Pongo una imagen de fondo,  que simula de momento la imagen de las tres botellas.
    El siguinte paso es insertar un boton encima del fondo

--- index.php
+++ index.php
@@ -1,24 +1,23 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<link rel="stylesheet" type="text/css" href="style.css" />
 	<meta charset="utf-8">
-	<title>Portada</title>
+	<title>Landing</title>
 	<?php session_start(); ?>
 </head>
 
 <body>
 
-	<div id="contenedor">
+	<div id="contenedor"> <!-- Contenedor-->
 
 		<?php require ('header.php'); ?>
 
 
-		<div class="container">
-			<!-- Aqui poneis lo que querais añadir de vuestra vista-->
+		<div class="container"><!--bloque del contenido central-->
+			<img id="fondoInicio" src="img/BackgroundInicio.JPG" alt="BackgroundInicio" />
 			<p> <a href="registrate.php"> Regístrate </a></p>
 		</div>
-		
-
 		<?php require('footer.php'); ?>
 
 	</div> <!-- Fin del contenedor -->
