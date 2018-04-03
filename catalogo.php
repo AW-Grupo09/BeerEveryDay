b8693b9a27a76395a5a8add0f1f0c63a19ff4e8b
juli89 <36840750+juli89@users.noreply.github.com>
2018-04-03 17:39:12 +0200

    filtro del catalogo

--- catalogo.php
+++ catalogo.php
@@ -1,5 +1,5 @@
 <?php 
-session_start();
+	session_start();
 ?>
 
 
@@ -23,25 +23,107 @@ session_start();
 					</div>
 				</header>
 				<section>
-					<form method="post">
-						<input type="text" placeholder="Nombre..." name="nombre"/>
-						<select name="pais">
-							<option value=""> Pais</option>
-							<?
-								while ($listaCervezas = $conexion->fetch_array(MYSQLI_BOTH))
-								{
-									echo '<option value="'.$listaCervezas[''].'">'.$registroCarreras['carrera'].'</option>';
-								}
-							?>
-						</select>
+					<form action="catalogo.php" method="post">
+					<?php
+						$sql = '';
+						echo '<fieldset>';
+						if (isset($_POST['artesana'])){
+							echo '<input type="checkbox" name="artesana" checked>Artesanas<br/>';
+							if(strcmp($sql, "") == 0){
+								$sql = $sql . 'where artesana = 1 ';
+							}else{
+								$sql = $sql . 'and artesana = 1 ';
+							}
+						}else{
+							echo '<input type="checkbox" name="artesana">Artesanas<br/>'; 
+						}
+						if (isset($_POST['nacional'])){
+							echo '<input type="checkbox" name="nacional" checked>Nacionales<br/>';
+							if(strcmp($sql, "") == 0){
+								$sql = $sql . 'where pais = "España" ';
+							}else{
+								$sql = $sql . 'and pais = "España" ';
+							}
+						}else{
+							echo '<input type="checkbox" name="nacional">Nacionales<br/>'; 
+						}
 
-						<select name="color">
-							<option value="">No. de Registros</option>
-							<option value="rubia">rubia</option>
-							<option value="tostada">tostada</option>
-							<option value="negra">negra</option>
-						</select>
-						<button name="buscar" type="submit">Buscar</button>
+						$grados = array("" => "", 
+										"Menor de 5" => " grado <= 5 ", 
+										"Entre 5 y 7" => " grado >= 5 and grado <= 7 ", 
+										"Mayor de 7" => " grado >= 7 ");
+						echo 'Grado Alcohólico: <select name="grado">';
+							foreach ($grados as $i => $v) {
+								if($_POST['grado'] == $i){
+									echo '<option value="'.  $i .'" selected="true">' . $i . '</option>';
+									if(strcmp($sql, "") == 0){
+										$sql = 'where ' . $v;
+									}else{
+										$sql = $sql . 'and ' . $v;
+									}
+								}else{
+									echo '<option value="'.  $i .'">' . $i . '</option>';
+								}
+							}
+						echo '</select>';
+						echo '</fieldset>';
+						//------------------------------------------------------------------------------
+						$colores = array("Rubia", "Negra", "Roja", "Tostada", "Blanca");
+						$sqlColor = '';
+						echo '<fieldset>';
+						echo '<legend>Color</legend>';
+						foreach ($colores as $i) {
+						    if (isset($_POST[$i])){
+								echo '<input type="checkbox" name="' . $i . '" checked>' . $i . '<br/>';
+								if(strcmp($sqlColor, "") == 0){
+									$sqlColor = $sqlColor . '(color = "' . $i . '" ';
+								}else{
+									$sqlColor = $sqlColor . 'or color = "' . $i . '" ';
+								}
+							}else{
+								echo '<input type="checkbox" name="' . $i . '">' . $i . '<br/>'; 
+							}
+						}
+						if(strcmp($sqlColor, "") != 0){
+							if(strcmp($sql, "") == 0){
+								$sql = 'where ' . $sqlColor . ') ';
+							}else{
+								$sql = $sql . 'and ' . $sqlColor . ') ';
+							}
+						}
+						echo '</fieldset>';
+						//----------------------------------------------------------------------------------
+						$granos = array("Cebada", "Trigo", "Avena");
+						$sqlGranos = '';
+						echo '<fieldset>';
+						echo '<legend>Ingredientes</legend>';
+						foreach ($granos as $i) {
+						    if (isset($_POST[$i])){
+								echo '<input type="checkbox" name="' . $i . '" checked>' . $i . '<br/>';
+								if(strcmp($sqlGranos, "") == 0){
+									$sqlGranos = $sqlGranos . '(grano = "' . $i . '" ';
+								}else{
+									$sqlGranos = $sqlGranos . 'or grano = "' . $i . '" ';
+								}
+							}else{
+								echo '<input type="checkbox" name="' . $i . '">' . $i . '<br/>'; 
+							}
+						}
+						if(strcmp($sqlGranos, "") != 0){
+							if(strcmp($sql, "") == 0){
+								$sql = 'where ' . $sqlGranos . ') ';
+							}else{
+								$sql = $sql . 'and ' . $sqlGranos . ') ';
+							}
+						}
+						echo '</fieldset>';
+
+
+						$sql = 'select * from cervezas ' . $sql;
+						echo $sql . '<br/>';
+						
+					?>
+					<input type="submit" name="buscar">
 					</form>
 				</section>
 			</div>
