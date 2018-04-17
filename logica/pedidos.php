28f7c159af6ce777c665d6e14744ad92acdd7b3f
juli89 <36840750+juli89@users.noreply.github.com>
2018-04-17 23:25:42 +0200

    clase pedidos

--- logica/pedidos.php
+++ logica/pedidos.php
@@ -1,6 +1,6 @@
 <?php
-class pedidos{
 
+class pedidos{
 	private $idPedido;
 	private $user;
 	private $grupo;
@@ -34,7 +34,7 @@ class pedidos{
 	}
 
 	public static eliminarCesta(){
-
+        //Esto debe realizarse despues de eliminar todos los elementos de la cesta
 		$cesta = loadCesta($_SESSION["user"]);
 		$query = "DELETE FROM  Pedidos WHERE idPedido = '$cesta'";
 		$correcto = $mysqli->query($query) or die ($mysqli->error . " en la línea " . (__LINE__-1));
@@ -42,135 +42,132 @@ class pedidos{
 
 	public static eliminarElementoCesta($cerveza){
 		//Esta funcion se encarga de eliminar un elemento de la cesta
-
-
+        $query = "DELETE FROM  `pedidos-cervezas` WHERE idcerveza = '" . $cerveza . "'  and idpedido = '" . $idPedido . "'";
+        $consulta = $mysqli->query($query) or die ($mysqli->error . " en la línea " . (__LINE__-1));
 	}
 
 	public static iniciarCesta($cerveza, $unidades){
-		//Esto inicializa un pedido, tan pronto como se añada un elemento a una cesta vacia
-		//php generate unic id.
+        //Obtiene el id mas alto y le suma 1 para un nuevo id
+        $sql = "SELECT MAX(idPedido) FROM pedidos";
+        $consulta = $mysqli->query($sql) or die ($mysqli->error. " en la línea ".(__LINE__-1));
+        $fila = mysqli_fetch_assoc($consulta);
+        $id = $fila["id"];
+        $id++;
+        //Se inicializa el pedido
+        $sql = "INSERT INTO pedidos(idPedido, estado) VALUES (" . $id . ",'cesta')";
+        $consulta = $mysqli->query($sql) or die ($mysqli->error . " en la línea " . (__LINE__-1));
+        $sql = "INSERT INTO `pedidos-cervezas`(`idCerveza`, `idPedido`, `unidades`) VALUES (" .  $cerveza . "," . $id . "," .  $unidades . ")";
+        $consulta = $mysqli->query($sql) or die ($mysqli->error . " en la línea " . (__LINE__-1));
 	}
 
 	private loadUser($idPedido){
-		//En funcion de un idPedido carga el usuario al que le corresponda el pedido
-
-
+		//Carga el usuario al que le corresponda el pedido
+        $sql = "SELECT idUsuario FROM usuarios-pedidos";
+        $consulta = $mysqli->query($sql) or die ($mysqli->error. " en la línea ".(__LINE__-1));
+        $fila = mysqli_fetch_assoc($consulta);
+        $user = $fila["idUsuario"];
 	}
 
 	private loadBeers($idPedido){
-		//Carga las cervezas y las unidades de un pedido
+		//Carga las cervezas y las unidades de un pedid
+        $sql = "SELECT idcerveza, unidades FROM `pedidos-cervezas` WHERE idpedido = ". $idPedido;
+        $consulta = $mysqli->query($sql) or die ($mysqli->error. " en la línea ".(__LINE__-1)); 
 
-
+        $i = 0;
+        while($fila= mysqli_fetch_assoc($consulta)){
+            $cervezas[$i] = $fila["idCerveza"];
+            $unidades[$i] = $fila["unidades"]
+            $i++;
+        }   
 	}
 
 	private loadGroup($idPedido){
-		//Carga el nombre del grupo al que pertenece el pedido
-
+		//Carga el id del grupo al que pertenece el pedido
+        $sql = "SELECT idGrupo FROM `grupos-pedidos`";
+        $consulta = $mysqli->query($sql) or die ($mysqli->error. " en la línea ".(__LINE__-1));
+        $fila = mysqli_fetch_assoc($consulta);
+        $idgrupo = $fila["idgrupo"];
 	}
 	private loadCesta($user){
-		//Devuelve el id de la cesta que corresponda al usuario
-
-
+		//Devuelve el idpedido de la cesta que corresponda al usuario
+        $sql = "SELECT pedidos.idpedido FROM pedidos, `usuarios-pedidos` WHERE pedidos.idPedido = `usuarios-pedidos`.`idPedido` and estado = 'cesta' and idusuario = '" .  $user . "'";
+        $consulta = $mysqli->query($sql) or die ($mysqli->error. " en la línea ".(__LINE__-1));
+        $fila = mysqli_fetch_assoc($consulta);
+        $idpedido = $fila["idpedido"];
 	}
 
-    public function getIdPedido()
-    {
+    public function getIdPedido(){
         return $this->idPedido;
     }
 
 
-    public function setIdPedido($idPedido)
-    {
+    public function setIdPedido($idPedido){
         $this->idPedido = $idPedido;
-
         return $this;
     }
 
-    public function getUser()
-    {
+    public function getUser(){
         return $this->user;
     }
 
-    public function setUser($user)
-    {
+    public function setUser($user){
         $this->user = $user;
-
         return $this;
     }
 
-    public function getGrupo()
-    {
+    public function getGrupo(){
         return $this->grupo;
     }
 
-    public function setGrupo($grupo)
-    {
+    public function setGrupo($grupo){
         $this->grupo = $grupo;
-
         return $this;
     }
 
-    public function getCervezas()
-    {
+    public function getCervezas(){
         return $this->cervezas;
     }
 
-    public function setCervezas($cervezas)
-    {
+    public function setCervezas($cervezas){
         $this->cervezas = $cervezas;
-
         return $this;
     }
 
-    public function getUnidades()
-    {
+    public function getUnidades(){
         return $this->unidades;
     }
 
-    public function setUnidades($unidades)
-    {
+    public function setUnidades($unidades){
         $this->unidades = $unidades;
-
         return $this;
     }
 
-    public function getDir()
-    {
+    public function getDir(){
         return $this->dir;
     }
 
-    public function setDir($dir)
-    {
+    public function setDir($dir){
         $this->dir = $dir;
-
         return $this;
     }
 
-    public function getTarjeta()
-    {
+    public function getTarjeta(){
         return $this->tarjeta;
     }
 
-    public function setTarjeta($tarjeta)
-    {
+    public function setTarjeta($tarjeta){
         $this->tarjeta = $tarjeta;
-
         return $this;
     }
 
-    public function getEstado()
-    {
+    public function getEstado(){
         return $this->estado;
     }
 
-    public function setEstado($estado)
-    {
+    public function setEstado($estado){
         $this->estado = $estado;
-
         return $this;
     }
 }
-
-
 
 ?>
