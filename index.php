cbaa250eaad1c333a1ad7a1d3a2eba9fabcb2420
Dario Gallegos <dariogal@ucm.es>
2018-04-22 21:51:45 +0200

    actualizo el index

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
 
 		<div id="cont01" class="container">
                   <div>
-                  	<img src="img/cervezalink1.jpg" alt="imagen cerveza"/>
+                  	<img src="img/cervezalink2.png" alt="imagen cerveza"/>
                   	<div class = "texto-centrado"><a href="http://localhost/BeerEveryDay/catalogo.php">Top sold beers</a></div>
                   </div>
                   <div>
@@ -23,7 +23,7 @@
                   	<div class = "texto-centrado"><a href="http://localhost/BeerEveryDay/catalogo.php">New arraivals</a></div>
                   </div>
                   <div>
-                  	<img src="img/cervezalink1.jpg" alt="imagen cerveza" />
+                  	<img src="img/cervezalink4.jpg" alt="imagen cerveza" />
                   	<div class = "texto-centrado"><a href="http://localhost/BeerEveryDay/catalogo.php">Groups</a></div>
                   </div>
             </div>
