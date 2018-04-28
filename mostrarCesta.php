9baee57f83816fec8d1c1710efe03f22189ca951
juli89 <36840750+juli89@users.noreply.github.com>
2018-04-28 12:55:18 +0200

    Corregir errores mostrarCesta.php

--- mostrarCesta.php
+++ mostrarCesta.php
@@ -1,8 +1,9 @@
 <?php 
-	include('logica/conexion.php');
+
 	include('logica/usuario.php');
 	include('logica/pedidos.php');
 	include('logica/cervezas.php');
+
 	if(!isset($_SESSION["logged"]))
 		$_SESSION["logged"] = false;
 	else{
@@ -16,10 +17,6 @@
 <head>
 	<meta charset="utf-8">
 	<link rel="stylesheet" type="text/css" href="css/common.css" />
-	<?php
-		$mysqli = conexion::getConection();
-		$user = new usuario($_SESSION["user"], $mysqli);
-	?>
 	<title>Cesta</title>
 
 </head>
@@ -32,63 +29,57 @@
 		<div class="container">
 			
 			<?php
-						$mysqli = conexion::getConection();
-						$idCesta = pedidos::loadCesta($user->getIdUsuario(),$mysqli);
+				$idCesta = pedidos::loadCesta($_SESSION["user"]);
 
-						if(isset($_POST['Eliminar'])){
-							pedidos::eliminarCesta($idCesta, $mysqli);
-							$cesta = null;
+				if(isset($_POST['Eliminar'])){
+					pedidos::eliminarCesta($idCesta);
+					$cesta = null;
+				}
+						
+				if($idCesta == null){
+					echo "<div><h1>Tu cesta está vacía.<h1></div>";
+				}else {
+					$cesta = new pedidos($idCesta);
+
+					foreach ($cesta->getCervezas() as $idCerveza) {
+						if(isset($_POST[$idCerveza])){
+							pedidos::eliminarElementoCesta($idCerveza, $idCesta);
+							$cesta = new pedidos($idCesta);
 						}
+					}
+					if(sizeof($cesta->getCervezas()) > 0) {
+						$cervezas = $cesta->getCervezas();
+						$unidades = $cesta->getUnidades();
+						$i = 0;
+						$total = 0;
 
-						if($idCesta == null){
-							echo "<div><h1>Tu cesta está vacía.<h1></div>";
-						}
-						else{
-							$cesta = new pedidos($idCesta, $mysqli);
-
-							foreach ($cesta->getCervezas() as $idCerveza) {
-								if(isset($_POST[$idCerveza])){
-									//echo "<p>" . $idCerveza . "</p>";
-									pedidos::eliminarElementoCesta($idCerveza, $idCesta, $mysqli);
-									$cesta = new pedidos($idCesta, $mysqli);
-								}
-							}
-							if(sizeof($cesta->getCervezas()) > 0)
-							{
-								$cervezas = $cesta->getCervezas();
-								$unidades = $cesta->getUnidades();
-								$i = 0;
-								$total = 0;
-
-								echo "<div>";
-								foreach ($cervezas as $idCerveza) {
-									$cerveza = new cervezas($idCerveza, $mysqli);
-									echo "<div>";
-										echo "<form action='mostrarCesta.php' method='post'>";
-										echo "<img alt='Imagen de cerveza' src=". $cerveza->getImagen()." width='200' height='200' />";
-										echo "<p>" . $cerveza->getNombre() . "</p>";
-										echo "<p>Precio unidad: " . $cerveza->getPrecio() . " €</p>";
-										echo "<p>Unidades: " . $unidades[$i] . "</p>";
-										echo "<p>Total: " . $cerveza->getPrecio() * $unidades[$i] . " €</p>";
-										$total = $total + ($cerveza->getPrecio()*$unidades[$i]);
-										echo "<input type='submit' name='" . $idCerveza . "' value='Eliminar'>";
-										echo "</form>";
-									echo "</div>";
-									$i++;
-								}
-								echo "<h1>Total: " . $total . " €</h1>";
+						echo "<div>";
+						foreach ($cervezas as $idCerveza) {
+							$cerveza = new cervezas($idCerveza);
+							echo "<div>";
 								echo "<form action='mostrarCesta.php' method='post'>";
-								echo "<input type='submit' name='Eliminar' value='Eliminar cesta'>";
+									echo "<img alt='Imagen de cerveza' src=". $cerveza->getImagen()." width='200' height='200' />";
+									echo "<p>" . $cerveza->getNombre() . "</p>";
+									echo "<p>Precio unidad: " . $cerveza->getPrecio() . " €</p>";
+									echo "<p>Unidades: " . $unidades[$i] . "</p>";
+									echo "<p>Total: " . $cerveza->getPrecio() * $unidades[$i] . " €</p>";
+									$total = $total + ($cerveza->getPrecio()*$unidades[$i]);
+									echo "<input type='submit' name='" . $idCerveza . "' value='Eliminar'>";
 								echo "</form>";
-								echo "</div>";
-							}
-							else
-							{
-								pedidos::eliminarCesta($idCesta, $mysqli);
-								echo "<div><h1>Tu cesta está vacía.<h1></div>";
-							}
+							echo "</div>";
+							$i++;
 						}
-					?>
+						echo "<h1>Total: " . $total . " €</h1>";
+						echo "<form action='mostrarCesta.php' method='post'>";
+								echo "<input type='submit' name='Eliminar' value='Eliminar cesta'>";
+						echo "</form>";
+						echo "</div>";
+					}else {
+						pedidos::eliminarCesta($idCesta);
+						echo "<div><h1>Tu cesta está vacía.<h1></div>";
+					}
+				}
+			?>
 		
 		</div>
 
