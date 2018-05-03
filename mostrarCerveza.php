3d898ae3d0f91127bdba53df673274b6091fd6ca
luisFSB <37073659+luisFSB@users.noreply.github.com>
2018-05-03 23:10:28 +0200

    mostrarCerveza.css y mostrarCerveza.php
    subir archivo mostrarCerveza.css y actualizar el php

--- mostrarCerveza.php
+++ mostrarCerveza.php
@@ -1,6 +1,7 @@
 <?php 
 	require_once __DIR__.'/includes/config.php';
-	include('logica/cervezas.php');
+	require_once __DIR__.'/includes/cervezas.php';
+	//include('logica/cervezas.php');
 	global $sql;
 ?>
 
@@ -8,6 +9,7 @@
 <html lang="es">
 <head>
 	<link rel="stylesheet" type="text/css" href="css/common.css" />
+	<link rel="stylesheet" type="text/css" href="css/mostrarCerveza.css" />
 	<meta charset="utf-8">
 	
 	<title>Cervezas</title>
@@ -31,19 +33,23 @@
 					
 			
 <?php
-			echo "<div>";
-			
+			echo "<div class= 'mostrarCerveza'>";
+			echo "<div class= 'nombreCerveza'>";
 			echo "<h1> ". $cerveza->getIdCerveza() . " - " . $cerveza->getNombre(). " </h1>";
-			echo "<img alt='Imagen de cerveza' src=". $cerveza->getImagen()." width='200' height='200' />";
-			echo "<p>" . " Capacidad : ". $cerveza->getCapacidad(). " Cl" ."</p>";
-			echo "<p>" . " Color : ". $cerveza->getColor() ."</p>";
-			echo "<p>" . " Tipo : ". $cerveza->getTipo() ."</p>";
-			echo "<p>". " Graduación : ". $cerveza->getGrado() . " % "."</p>";
-			echo "<p>". " Ingredientes : ". $cerveza->getGrano() ."</p>";
-			echo "<p>"." País : " . $cerveza->getPais()."</p>";
-			echo "<p>" . " Precio : ". $cerveza->getPrecio(). " € ". "</p>";
-			
-			echo "</div>";
+			echo "</div>";// cierro div nombre
+			echo "<div class= 'contenidoCerveza'>";
+			echo "<div class= 'imagenCerveza'>";
+			echo "<img alt='Imagen de cerveza' src=". $cerveza->getImagen()." width='300' height='300' />";
+			echo "</div>";// cierro div imagen
+			echo "<div class= 'datosCerveza'>";
+			echo "<p>" . " <span>Capacidad : </span>". $cerveza->getCapacidad(). " Cl" ."</p>";
+			echo "<p>" . " <span>Color : </span>". $cerveza->getColor() ."</p>";
+			echo "<p>" . " <span>Tipo : </span>". $cerveza->getTipo() ."</p>";
+			echo "<p>". " <span>Graduación : </span>". $cerveza->getGrado() . " % "."</p>";
+			echo "<p>". " <span>Ingredientes : </span>". $cerveza->getGrano() ."</p>";
+			echo "<p>"." <span>País : </span>" . $cerveza->getPais()."</p>";
+			echo "<p>" . " <span>Precio : </span>". $cerveza->getPrecio(). " € ". "</p>";
+			echo "</div>";//cierro div datosCerveza
 
 			if(isset($_SESSION['login']) && $_SESSION['login']){
 				echo '<form  action="logica/procesarCesta.php" method="GET">';
@@ -60,13 +66,16 @@
 								}
 							}
 						echo '</select>';
-			*/	echo '<input type="number" name="unidades" min=1 placeholder="unidades">';
+			*/	echo '<input type="number" name="unidades" min=1 placeholder="Unidades">';
 				//echo '<input type="submit" name="cerveza" value="'. $cerveza->getIdCerveza().'" placeholder="Añadir a la cesta""> ';
-				echo '<button class="submit" type="submit" name="cerveza" value="'. $cerveza->getIdCerveza().'">Añadir a la cesta</button>';
+				echo '<button class="submit" type="submit" name="cerveza" value="'. $cerveza->getIdCerveza().'">Añadir a la cesta
+				</button>';
 				echo '</form>';
 			}
+			echo "</div>";//cierro div contenidoCerveza
+			echo "</div>";//cierro div mostrarCerveza
 ?>
-		</div>
+		</div><!-- Fin del container -->
 
 		<?php require('includes/comun/footer.php'); ?>
 
