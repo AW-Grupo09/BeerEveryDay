ca7f7d2d2b71cba6790b911c5b6bf9934905e934
endela01 <endela01@ucm.es>
2018-05-04 03:01:18 +0200

    .

--- includes/comun/footer.php
+++ includes/comun/footer.php
@@ -1,66 +1,58 @@
 
 
 <div class="footer">
+	<div class="footer-container">	
+		<div class="footer-main">
+			<div class="footer-columna">
+				<h3>ATENCIÓN AL CLIENTE</h3>
+					<p>
+						<a href ='contacto.php'>Contáctenos</a>
+					</p>
 
-		<div class="footer-container">	
+					<span class="fa fa-phone">
+						<p>(+34) 910 000 000</p>
+					</span>
 
-				<div class="footer-main">
-					<div class="footer-columna">
-							<h3>ATENCIÓN AL CLIENTE</h3>
+					<span class="fa fa-envelope">
+						<p>info@beereveryday.com</p>
+					</span>
+			</div>
 
-								<p>
-									<a href ='contacto.php'>Contáctenos</a>
-								</p>
-								<span class="fa fa-phone">
-									<p>(+34) 910 000 000</p>
-								</span>
+			<div class="footer-columna">
+				<h3>SUSCRÍBETE</h3>
+					<input type="email" placeholder="Escríba su correo">
+					<input type="submit" value="Suscribirse">
+			</div>
 
-								<span class="fa fa-envelope">
-									<p>info@beereveryday.com</p>
-								</span>
+			<div class="footer-columna">
+				<h3>SOBRE NOSOTROS</h3>
+					<p>
+						<a href ='aboutus.php'>¿Quienes somos?</a>
+					</p>
 
-								
-					</div>
+					<p>
+						<a href ='faq.php'>Preguntas frecuentes</a>
+					</p>
 
-
-					<div class="footer-columna">
-							<h3>SUSCRÍBETE</h3>
-								<input type="email" placeholder="Escríba su correo">
-								<input type="submit" value="Suscribirse">
-					</div>
-
-
-					<div class="footer-columna">
-							<h3>SOBRE NOSOTROS</h3>
-								<p>
-									<a href ='aboutus.php'>¿Quienes somos?</a>
-								</p>
-
-								<p>
-									<a href ='faq.php'>Preguntas frecuentes</a>
-								</p>
-
-					</div>	
-				</div>
-
-
-				<div class="footer-copy-redes">
-						<div class="main-copy-redes">
-								<div class="footer-copy">
-									<p>© 2018. Todos los derechos reservados - |BeerEveryDay|.</p>
-								</div>
-
-								<div class="footer-redes">
-									<a href="#" class="fa fa-facebook"> </a>
-									<a href="#" class="fa fa-twitter"> </a>
-									<a href="#" class="fa fa-youtube-play"> </a>
-									<a href="#" class="fa fa-github"> </a>
-								</div>
-
-						</div>
-				</div>
+			</div>	
 		</div>
-		
+
+
+		<div class="footer-copy-redes">
+			<div class="main-copy-redes">
+				<div class="footer-copy">
+					<p>© 2018. Todos los derechos reservados - |BeerEveryDay|.</p>
+				</div>
+
+				<div class="footer-redes">
+					<a href="#" class="fa fa-facebook"> </a>
+					<a href="#" class="fa fa-twitter"> </a>
+					<a href="#" class="fa fa-youtube-play"> </a>
+					<a href="#" class="fa fa-github"> </a>
+				</div>
+			</div>
+		</div>
+	</div>	
 </div>
 
 
