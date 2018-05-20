10b055a212cb67da3fe6ab8b8f4b437c0af92a36
endela01 <endela01@ucm.es>
2018-05-20 18:35:32 +0200

    VistaGruposPrimerEnfoque

--- includes/grupos.php
+++ includes/grupos.php
@@ -1,78 +1,123 @@
 <?php
 require_once __DIR__ . '/Aplicacion.php';
 
-Class Grupos{
-
-
-
-    public static function buscaGrupo($nombreGrupo){
+class Grupos
+{
+    public static function getGruposByUser($usuario)
+    {
+        $grupos = array();
         $app = Aplicacion::getSingleton();
         $conn = $app->conexionBd();
-        $query = sprintf("SELECT * FROM grupos WHERE nombre = '$nombreGrupo'", $conn->real_escape_string($nombreGrupo));
-        $rs = $conn->query($query);   
-        $result = false;
+        $query = sprintf("SELECT * FROM grupos WHERE creador = '%s'", $conn->real_escape_string($usuario));
+        $rs = $conn->query($query);
         if ($rs) {
-            if ( $rs->num_rows == 1) {
-                $fila = $rs->fetch_assoc();
-                $grupo = new Grupos($fila['id'],$fila['nombre'], $fila['direccion'], $fila['cuidad']);
-                $result = $grupo;
+            while ($fila = $rs->fetch_assoc()) {
+                $grupo = new Grupos($fila['nombre'], $fila['direccion'], $fila['creador'], $fila['ciudad']);
+                $grupo->setId($fila['id']);
+                array_push($grupos, $grupo);
             }
             $rs->free();
         } else {
             echo "Error al consultar en la BD: (" . $conn->errno . ") " . utf8_encode($conn->error);
             exit();
         }
-        return $result;
+        return $grupos;
     }
-    
-    public static function creaGrupo($nombre, $direccion,$ciudad){
+
+    public static function getGrupos()
+    {
+        $grupos = array();
+        $app = Aplicacion::getSingleton();
+        $conn = $app->conexionBd();
+        $query = sprintf("SELECT * FROM grupos");
+        $rs = $conn->query($query);
+        if ($rs) {
+            while ($fila = $rs->fetch_assoc()) {
+                $grupo = new Grupos($fila['nombre'], $fila['direccion'], $fila['creador'], $fila['ciudad']);
+                $grupo->setId($fila['id']);
+                array_push($grupos, $grupo);
+            }
+            $rs->free();
+        } else {
+            echo "Error al consultar en la BD: (" . $conn->errno . ") " . utf8_encode($conn->error);
+            exit();
+        }
+        return $grupos;
+    }
+
+    public static function buscaGrupo($nombreGrupo)
+    {
+        $app = Aplicacion::getSingleton();
+        $conn = $app->conexionBd();
+        $query = sprintf("SELECT * FROM grupos WHERE nombre = '%s'", $conn->real_escape_string($nombreGrupo));
+        $rs = $conn->query($query);
+        $grupo = false;
+        if ($rs) {
+            if ($rs->num_rows == 1) {
+                $fila = $rs->fetch_assoc();
+                $grupo = new Grupos($fila['nombre'], $fila['direccion'], $fila['creador'], $fila['ciudad']);
+                $grupo->setId($fila['id']);
+            }
+            $rs->free();
+        } else {
+            echo "Error al consultar en la BD: (" . $conn->errno . ") " . utf8_encode($conn->error);
+            exit();
+        }
+        return $grupo;
+    }
+
+    public static function creaGrupo($nombre, $direccion, $ciudad)
+    {
         $grupo = self::buscaGrupo($nombre);
         if ($grupo) {
             return false;
         }
-        $grupo = new Grupos($nombre,$direccion,$ciudad);
+        $grupo = new Grupos($nombre, $direccion, $_SESSION['nombreUsuario'], $ciudad);
         return self::inserta($grupo);
     }
 
-    public static function insetaGrupoUsuarios($idUsuario,$idGrupo,$admin){
-    	$app = Aplicacion::getSingleton();
+    public static function insetaGrupoUsuarios($idUsuario, $idGrupo, $admin)
+    {
+        $app = Aplicacion::getSingleton();
         $conn = $app->conexionBd();
-        $query=sprintf("INSERT INTO 'grupos-usuarios' VALUES('%s', '%s', '%s')"
-            , $conn->real_escape_string($idGrupo)
-            , $conn->real_escape_string($idUsuario)
-            , $conn->real_escape_string($admin)
-            );
+        $query = sprintf(
+            "INSERT INTO `grupos-usuarios` VALUES('%s', '%s', '%s')",
+            $conn->real_escape_string($idGrupo),
+            $conn->real_escape_string($idUsuario),
+            $conn->real_escape_string($admin)
+        );
 
-        if ( $conn->query($query) ) {
+        if ($conn->query($query)) {
             $grupoUsuario = $conn->insert_id;
         } else {
-
             echo "Error al insertar en la BD: (" . $conn->errno . ") " . utf8_encode($conn->error);
             exit();
         }
         return $grupoUsuario;
-
     }
 
-    public static function actualizaGrupo($nombre,$direccion,$cuidad){
+    public static function actualizaGrupo($nombre, $direccion, $ciudad)
+    {
         $grupo = self::buscaGrupo($nombre);
         if (!$grupo) {
             return false;
         }
-        $grupo = new Grupos($nombre,$direccion,$ciudad);
+        $grupo = new Grupos($nombre, $direccion, $ciudad);
         return self::actualiza($grupo);
     }
 
-    private static function inserta($grupo){
-
+    private static function inserta($grupo)
+    {
         $app = Aplicacion::getSingleton();
         $conn = $app->conexionBd();
-        $query=sprintf("INSERT INTO grupos(nombre, direccion, ciudad) VALUES('%s', '%s', '%s')"
-            , $conn->real_escape_string($grupo->nombre)
-            , $conn->real_escape_string($grupo->direccion)
-            , $conn->real_escape_string($grupo->ciudad)
-            );
-        if ( $conn->query($query) ) {
+        $query = sprintf(
+            "INSERT INTO grupos(nombre, direccion, creador, ciudad) VALUES('%s', '%s', '%s', '%s')",
+            $conn->real_escape_string($grupo->nombre),
+            $conn->real_escape_string($grupo->direccion),
+            $conn->real_escape_string($grupo->creador),
+            $conn->real_escape_string($grupo->ciudad)
+        );
+        if ($conn->query($query)) {
             $grupo->id = $conn->insert_id;
         } else {
             echo "Error al insertar en la BD: (" . $conn->errno . ") " . utf8_encode($conn->error);
@@ -80,18 +125,21 @@ Class Grupos{
         }
         return $grupo;
     }
-    
 
-    private static function actualiza($grupo){
+
+    private static function actualiza($grupo)
+    {
         $app = Aplicacion::getSingleton();
         $conn = $app->conexionBd();
-        $query=sprintf("UPDATE grupo G SET nombre = '%s', direccion='%s', ciudad='%s' WHERE G.id=%i"
-            , $conn->real_escape_string($grupo->nombre)
-            , $conn->real_escape_string($grupo->direccion)
-            , $conn->real_escape_string($grupo->ciudad)
-            , $grupo->id);
-        if ( $conn->query($query) ) {
-            if ( $conn->affected_rows != 1) {
+        $query=sprintf(
+            "UPDATE grupo G SET nombre = '%s', direccion='%s', ciudad='%s' WHERE G.id=%i",
+            $conn->real_escape_string($grupo->nombre),
+            $conn->real_escape_string($grupo->direccion),
+            $conn->real_escape_string($grupo->ciudad),
+            $grupo->id
+        );
+        if ($conn->query($query)) {
+            if ($conn->affected_rows != 1) {
                 echo "No se ha podido actualizar el grupo: " . $grupo->id;
                 exit();
             }
@@ -99,15 +147,15 @@ Class Grupos{
             echo "Error al insertar en la BD: (" . $conn->errno . ") " . utf8_encode($conn->error);
             exit();
         }
-        
+
         return $grupo;
     }
 
-
-	private function __construct($nombre, $direccion, $cuidad){
-
+    private function __construct($nombre, $direccion, $creador, $ciudad)
+    {
         $this->nombre = $nombre;
         $this->direccion = $direccion;
+        $this->creador = $creador;
         $this->ciudad = $ciudad;
     }
 
@@ -115,23 +163,35 @@ Class Grupos{
 
     private $direccion;
 
-    private $cuidad;
+    private $creador;
+
+    private $ciudad;
 
     private $id;
 
 
-    public function getNombre(){
+    public function getNombre()
+    {
         return $this->nombre;
     }
-    public function getDireccion(){
+    public function getDireccion()
+    {
         return $this->direccion;
     }
-    public function getCuidad(){
-        return $this->cuidad;
+    public function getCreador()
+    {
+        return $this->creador;
     }
-    public function getId(){
+    public function getCiudad()
+    {
+        return $this->ciudad;
+    }
+    public function getId()
+    {
         return $this->id;
     }
-
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
 }
-?>
--- mostrarGrupos.php
+++ mostrarGrupos.php
@@ -1,44 +1,97 @@
 <?php
-	require_once __DIR__.'/includes/config.php';
-	require_once __DIR__.'/includes/FormularioGrupo.php';
+    require_once __DIR__.'/includes/config.php';
+    require_once __DIR__.'/includes/FormularioGrupo.php';
 ?>
 
 <!DOCTYPE html>
 <html lang="es">
 <head>
-	<meta charset="utf-8">
-	<title> Grupos </title>
-	<link rel="stylesheet" type="text/css" href="css/common.css">
-	<link rel="stylesheet" type="text/css" href="css/footer.css"/>
+    <meta charset="utf-8">
+    <title> Grupos </title>
+    <link rel="stylesheet" type="text/css" href="css/common.css">
+    <link rel="stylesheet" type="text/css" href="css/footer.css"/>
 </head>
 
 <body>
+    <?php
+    if (isset($_GET['action']) && $_GET['action'] == 'unirse') {
+        Grupos::insetaGrupoUsuarios($_SESSION['nombreUsuario'], $_GET['id'], 0);
+    }
+    ?>
 
-	<div id="contenedor">
+    <div id="contenedor">
 
-		<?php require ('includes/comun/header.php'); ?>
+        <?php require('includes/comun/header.php'); ?>
 
-		<div class="container">
-			<div class="titulo">
-				<p><h1> ¡Mis grupos! </h1></p>
-		  	</div>
+        <div class="container">
+            <div class="titulo">
+                <p><h1> ¡Mis grupos! </h1></p>
+            </div>
 
-		   <?php 
+            <table>
+                <thead>
+                    <th>Nombre</th>
+                    <th>Direccion</th>
+                    <th>Ciudad</th>
+                    <th>Creador</th>
+                </thead>
+                <?php
+                $grupos = Grupos::getGruposByUser($_SESSION['nombreUsuario']);
+                foreach ($grupos as $grupo) { ?>
+                <tr>
+                    <td><?=$grupo->getNombre()?></td>
+                    <td><?=$grupo->getDireccion()?></td>
+                    <td><?=$grupo->getCreador()?></td>
+                    <td><?=$grupo->getCiudad()?></td>
+                </tr>
+                <?php } ?>
+            </table>
 
-				$opciones = array();
+            <?php
 
-				$formulario = new FormularioGrupo("formGrupo", $opciones);
-				$formulario->gestiona();
+                $opciones = array();
 
-			?>
+                $formulario = new FormularioGrupo("formGrupo", $opciones);
+                $formulario->gestiona();
 
-		</div>
+            ?>
+
+            <div class="titulo">
+                <p><h1> ¡Otros grupos! </h1></p>
+            </div>
+
+            <table>
+                <thead>
+                    <th>Nombre</th>
+                    <th>Direccion</th>
+                    <th>Ciudad</th>
+                    <th>Creador</th>
+                    <th></th>
+                </thead>
+                <?php
+                $grupos = Grupos::getGrupos();
+                foreach ($grupos as $grupo) { ?>
+                <tr>
+                    <td><?=$grupo->getNombre()?></td>
+                    <td><?=$grupo->getDireccion()?></td>
+                    <td><?=$grupo->getCiudad()?></td>
+                    <td><?=$grupo->getCreador()?></td>
+                    <td><button onclick="unirse(<?=$grupo->getId()?>)">Unirse</button></td>
+                </tr>
+                <?php } ?>
+            </table>
+
+        </div>
 
 
-		<?php require('includes/comun/footer.php'); ?>
+        <?php require('includes/comun/footer.php'); ?>
 
-	</div> <!-- Fin del contenedor -->
-	
+    </div> <!-- Fin del contenedor -->
 
+    <script>
+        function unirse(id){
+            window.location = "mostrarGrupos.php?action=unirse&id=" + id;
+        }
+    </script>
 </body>
 </html>
